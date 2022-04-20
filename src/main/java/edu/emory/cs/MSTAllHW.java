1ef81ee730cc7a10b77076b4e409bf96d765226a
Chau-Tran <78325991+Chau-Tran@users.noreply.github.com>
2022-04-19 21:38:10 -0400

    HW3

--- src/main/java/edu/emory/cs/MSTAllHW.java
+++ src/main/java/edu/emory/cs/MSTAllHW.java
@@ -24,62 +24,70 @@ import java.util.*;
 public class MSTAllHW implements MSTAll {
 
     List<SpanningTree> mst = new ArrayList<>();
+    List<List<String>> canonTrees = new ArrayList<>();
 
     @Override
     public List<SpanningTree> getMinimumSpanningTrees(Graph graph) {
-
-        List<SpanningTree> trees = new ArrayList<>();
-        PriorityQueue<Edge> queue = new PriorityQueue<>();
-        SpanningTree spanningTree = new SpanningTree();
-        Set<Integer> visited = new HashSet<>();
-
-        double minWeight = new MSTPrim().getMinimumSpanningTree(graph).getTotalWeight(); // all mst should have this weight, any trees weighing more is not mst
-
-        add(queue, visited, graph, 0);
-        mstAux(graph, trees, queue, spanningTree, visited); // adds all spanning trees into trees list
-
-        for (SpanningTree tree : trees) {
-
-            HashSet<Integer> vertex = new HashSet<>();
-
-            if (tree.getTotalWeight() != minWeight) continue; //not mst
-
-            for (Edge edge : tree.getEdges()) {
-                vertex.add(edge.getTarget());
-                vertex.add(edge.getSource());
-            }
-            if (vertex.size() != graph.size()) continue; //not mst
-
-            mst.add(tree); // passes conditions: is an mst!
+        if (graph.getAllEdges().size() == 0) {
+            System.out.println(mst.size());
+            return mst;
         }
-        return mst; // list of all mst's
+        mstAux(0, graph, new PriorityQueue<>(), new SpanningTree(), new HashSet<>());
+        System.out.println(mst.size());
+
+//        MSTPrim min = new MSTPrim();
+//        mst.add(min.getMinimumSpanningTree(graph));
+//        System.out.println(mst);
+
+        return mst;
+
     }
 
-    public void mstAux(Graph graph, List<SpanningTree> trees, PriorityQueue<Edge> queue, SpanningTree spanningTree, Set<Integer> visited) {
-        while (!queue.isEmpty()) {
+    public List<String> redundant(SpanningTree t1) {
+        List<String> canon = new ArrayList<>();
 
-            SpanningTree altTree = new SpanningTree(spanningTree);
-            Set<Integer> altVisited = new HashSet<>(visited);
+        for (Edge e : t1.getEdges()) {
+            String str = new String();
 
-            List<Edge> edges = new ArrayList<>();
-            Edge edge = queue.poll();
-            edges.add(edge);
+            if (e.getSource() > e.getTarget()) {
+                str += e.getTarget() + " - " + e.getSource();
+            } else {
+                str += e.getSource() + " - " + e.getTarget();
+            }
+            canon.add(str);
+        }
+        Collections.sort(canon);
+        return canon;
+    }
+
+    public void mstAux(int vertex, Graph graph, PriorityQueue<Edge> queue, SpanningTree tree, HashSet<Integer> visited) {
+
+        add(queue, visited, graph, vertex);
+        List<Edge> diverges = new ArrayList<>();
+
+        double min;
+        if(!queue.isEmpty()) min = queue.peek().getWeight();
+        else return;
+
+        for (Edge e : queue) {
+            if (e.getWeight() == min) diverges.add(e);
+        }
+
+        for (Edge e : diverges) {
 
             PriorityQueue<Edge> altQ = new PriorityQueue<>(queue);
+            altQ.remove(e);
 
-            while (!altQ.isEmpty()) {
-                Edge altEdge = altQ.poll();
-                if (edge.getWeight() == altEdge.getWeight()) edges.add(edge);
-            }
-
-            if (!visited.contains(edge.getSource())) {
-                PriorityQueue<Edge> tempQ = new PriorityQueue<>(queue);
-                altTree.addEdge(edge);
-
-                if (altTree.size() + 1 == graph.size()) trees.add(altTree); //a spanning tree was found
-
-                add(tempQ, altVisited, graph, edges.get(0).getSource());
-                mstAux(graph, trees, tempQ, altTree, altVisited); // recurse
+            if (!visited.contains(e.getSource())) {
+                SpanningTree altTree = new SpanningTree(tree);
+                altTree.addEdge(e);
+                if (altTree.size() + 1 == graph.size()) {
+                    if (!canonTrees.contains(redundant(altTree))) {
+                        canonTrees.add(redundant(altTree));
+                        mst.add(altTree);
+                    }
+                }
+                mstAux(e.getSource(), graph, altQ, altTree, new HashSet<>(visited));
             }
         }
     }
@@ -90,94 +98,5 @@ public class MSTAllHW implements MSTAll {
             if (!visited.contains(edge.getSource()))
                 queue.add(edge);
         }
-
     }
-
-//    public static void main(String[] args) {
-//        Graph graph = new Graph(4);
-//        graph.setUndirectedEdge(0, 1, 2);
-//        graph.setUndirectedEdge(1, 2, 2);
-//        graph.setUndirectedEdge(2, 3, 2);
-//        graph.setUndirectedEdge(3, 0, 2);
-//
-//        MSTAll gold = new MSTAllHW();
-//        System.out.println("\n" + gold.getMinimumSpanningTrees(graph));
-//    }
 }
-
-//    PriorityQueue<Edge> queue = new PriorityQueue<>();
-//        SpanningTree tree = new SpanningTree();
-//        Set<Integer> visited = new HashSet<>();
-//        Edge edge;
-//
-//        add(queue, visited, graph, 0);
-//
-//        while(!queue.isEmpty()){
-//
-//            edge = queue.poll();
-//
-//            PriorityQueue<Edge> altQueue = new PriorityQueue<>(queue);
-//            Edge alt = altQueue.peek();
-//            altQueue.add(edge);
-//
-//
-//            if(alt != null && edge.getWeight()== alt.getWeight()){
-//
-//                alt = altQueue.poll();
-//
-//                System.out.println("alt: " + alt + " altQ: " + altQueue);
-//                System.out.println("edge: " + edge + " Q: " + queue);
-//
-//                mstAux(graph, edge,queue, new SpanningTree(), new HashSet<>(visited));
-//                mstAux(graph, alt,altQueue, new SpanningTree(tree), new HashSet<>(visited));
-//            }
-//            else{
-//                mstAux(graph, edge, queue, new SpanningTree(tree), new HashSet<>(visited));
-//            }
-//        }
-//        return mst;
-//    }
-//
-//    public void mstAux(Graph graph, Edge edge, PriorityQueue<Edge> queue, SpanningTree tree, HashSet<Integer> visited){
-//        tree.addEdge(edge);
-//        System.out.println("\nedge: " + edge + " ||| Q: " + queue + " ||| tree: " + tree + " ||| visited: " + visited);
-//
-//        add(queue, visited, graph, edge.getSource());
-//        System.out.println("\nvisited: " + visited + " ||| queue: "+queue);
-//
-//
-//        while(!queue.isEmpty()){
-//
-//            edge = queue.poll();
-//
-//            Edge alt = queue.peek();
-//            PriorityQueue<Edge> altQueue = new PriorityQueue<>(queue);
-//            altQueue.add(edge);
-//
-//            if(alt != null && edge.getWeight() == alt.getWeight()){
-//                System.out.println("F");
-//                alt = altQueue.poll();
-//
-//                mstAux(graph, edge, queue, new SpanningTree(tree), new HashSet<>(visited));
-//                mstAux(graph, alt, altQueue, new SpanningTree(tree), new HashSet<>(visited));
-//            }
-//
-//            else{
-//                System.out.println("T");
-//
-//                if(!visited.contains(edge.getSource())){
-//                    tree.addEdge(edge);
-//                    if(tree.size() +1 == graph.size()){
-//                        System.out.println("mst! " + tree);
-//                        mst.add(tree);
-//                        break;
-//                    }
-//                    add(queue, visited, graph, edge.getSource());
-//                }
-//                else if(tree.size() +1 == graph.size()){
-//                    System.out.println("mst! " + tree);
-//                    mst.add(tree);
-//                    break;
-//                }
-//            }
-//        }
